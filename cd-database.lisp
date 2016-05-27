3bdfb5bf041730b521e45c4c233a44646541c285
sbogdanchuk <sbogdanchuk@localhost>
2016-05-27 18:44:12 +0300

    format use

--- cd-database.lisp
+++ cd-database.lisp
@@ -2,3 +2,8 @@
  (list :title title :artist artist :rating rating :ripped ripped))
 (defvar *db* nil)
 (defun add-record (cd) (push cd *db*))
+(defun dump-db ()
+ (dolist (cd *db*)
+   (format t "~{~a:~10t~a~%~}~%" cd)))
+(defun dump-db2 ()
+ (format t "~{~{~a:~10t~a~%~}~%~}" *db*))
