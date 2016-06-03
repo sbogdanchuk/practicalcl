12dec19cb5e1f447280cbce0de825077ef3b891d
sbogdanchuk <sbogdanchuk@localhost>
2016-06-03 16:20:04 +0300

    making progress )

--- cd-database.lisp
+++ cd-database.lisp
@@ -7,3 +7,13 @@
    (format t "~{~a:~10t~a~%~}~%" cd)))
 (defun dump-db2 ()
  (format t "~{~{~a:~10t~a~%~}~%~}" *db*))
+(defun prompt-read (prompt)
+ (format *query-io* "~a: " prompt)
+ (force-output *query-io*)
+ (read-line *query-io*))
+(defun prompt-for-cd ()
+ (make-cd 
+  (prompt-read "Title")
+  (prompt-read "Artist")
+  (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+  (y-or-n-p "Ripped [y/n] ")))
